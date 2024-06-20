694e8bd6ff063809fc22ffb219d8b3138e84a2fe
costa-victor <victorengcomp15@gmail.com>
2024-06-20 15:05:51 -0300

    fix: add the cmake config file generation for the someLibA

--- someLibA/cmake/Installing.cmake
+++ someLibA/cmake/Installing.cmake
@@ -45,24 +45,24 @@ install(EXPORT "${PROJECT_NAME}Targets"
     DESTINATION cmake
 )
 
-# # # include(CMakePackageConfigHelpers)
+include(CMakePackageConfigHelpers)
 
-# # # # generate the version file for the config file
-# # # write_basic_package_version_file(
-# # #     "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
-# # #     COMPATIBILITY AnyNewerVersion
-# # # )
-# # # # create config file
-# # # configure_package_config_file(${CMAKE_CURRENT_SOURCE_DIR}/Config.cmake.in
-# # #     "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake"
-# # #     INSTALL_DESTINATION cmake
-# # # )
-# # # # install config files
-# # # install(FILES
-# # #     "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake"
-# # #     "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
-# # #     DESTINATION cmake
-# # # )
+# generate the version file for the config file
+write_basic_package_version_file(
+    "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
+    COMPATIBILITY AnyNewerVersion
+)
+# create config file
+configure_package_config_file(${CMAKE_CURRENT_SOURCE_DIR}/Config.cmake.in
+    "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake"
+    INSTALL_DESTINATION cmake
+)
+# install config files
+install(FILES
+    "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake"
+    "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
+    DESTINATION cmake
+)
 # generate the export targets for the build tree
 # (can't say what this one is for, but so far it has been only causing me problems)
 # export(EXPORT "${PROJECT_NAME}Targets"
