963984495f2a81f800c101555657916e7277667b
costa-victor <victorengcomp15@gmail.com>
2024-06-22 13:17:54 -0300

    feat: add some more messages in the libC

--- someLibC/src/someLibC.cpp
+++ someLibC/src/someLibC.cpp
@@ -3,10 +3,11 @@
 
 namespace libC {
     SomeLibC::SomeLibC() {
+        std::cout << "Hello from libC constructor ::: " << std::endl;
         version = 42;
     }
     void SomeLibC::abstractPublicHello() {
-        std::cout << "Hello from the abstract public function ::: " << getVersion() << std::endl;
+        std::cout << "Hello from the abstract public function in libC ::: " << getVersion() << std::endl;
     }
     int SomeLibC::getVersion() {
         return version;
